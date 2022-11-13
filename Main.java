5710ccf37bcaa351e7ad0303b954d663d3bf000f
Artur Penar <artur.riczard.penar@gmail.com>
2022-11-13 10:04:26 +0100

    Main.java przetestowany wprowadzone zmiany.

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         DataManager dataManager = new DataManager();
-        dataManager.selectAllItems();
-        dataManager.createTable();
-        dataManager.insertData("youtube", "youtube.com", "KabanYoutuber", "YT");
-        DataRow dataRow = new DataRow(1, "google", "artur@gmail.com", "kaban", "developer", "gmail.com");
+        // dataManager.selectAllItems();
+        // dataManager.createTable();
+        // dataManager.insertData("youtube", "youtube.com", "KabanYoutuber", "YT");
+        DataRow dataRow = dataManager.getByApp("");
         dataRow.displayRow(); 
+        dataManager.selectAllItems();
+         
   }
-
 }
